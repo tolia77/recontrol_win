bf352954db6f039bf418a957f76df8d1913dd5f3
tolia k <tolia.kohan@gmail.com>
2025-10-20 17:01:38 +0300

    Add project files.

---
+++ recontrol_win/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace recontrol_win
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
